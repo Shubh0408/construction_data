1e2dd2ba5df325168f4dcfff7bda851ba77f4931
shubhamverma9982 <104002749+shubhamverma9982@users.noreply.github.com>
2023-05-05 16:38:10 +0530

    Add files via upload
    intial commit

---
+++ construction_data/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Home | Registration and Login System </title>
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <a class="navbar-brand ps-3" href="index.php">Construction Company</a>          
+        </nav>
+        <div id="layoutSidenav">
+       
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">  User and Vendor registration</h1>
+        <div class="row" >
+        <div class="col-xl-4 col-md-6" >
+                                <div class="card bg-primary text-white mb-4">
+                                    <div class="card-body">Signup as User</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="signup.php">Signup Here</a>
+                                        <div class="small text-white"></div>
+                                    </div>
+                                </div>
+                            </div><div class="col-xl-4 col-md-6" >
+                                <div class="card bg-primary text-white mb-4">
+                                    <div class="card-body">Signup as Vendor</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="signup_vender.php">Signup Here</a>
+                                        <div class="small text-white"></div>
+                                    </div>
+                                </div>
+                            </div>
+<div class="col-xl-4 col-md-6">
+                                <div class="card bg-warning text-white mb-4">
+                                    <div class="card-body">Login As User</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="login.php">Login Here</a>
+                                    </div>
+                                </div>
+                            </div>
+
+<div class="col-xl-4 col-md-6">
+                                <div class="card bg-danger text-white mb-4">
+                                    <div class="card-body">Login as Vendor</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="login_vendor.php">Login Here</a>
+                          
+                                    </div>
+                                </div>
+                            </div>
+
+                            </div>
+                        <div style="height: 100vh"></div>
+            
+                    </div>
+                </main>
+   <?php include_once('include/footer.php');?>
+            </div>
+        </div>
+
+    </body>
+</html>
---
+++ construction_data/login.php
@@ -0,0 +1,84 @@
+<?php session_start(); 
+include_once('include/config.php');
+// Code for login 
+if(isset($_POST['login']))
+{
+$password=$_POST['password'];
+$dec_password=$password;
+$useremail=$_POST['uemail'];
+$ret= mysqli_query($con,"SELECT id,fname FROM users WHERE email='$useremail' and password='$dec_password'");
+$num=mysqli_fetch_array($ret);
+if($num>0)
+{
+
+$_SESSION['id']=$num['id'];
+$_SESSION['name']=$num['fname'];
+header("location:welcome.php");
+
+}
+else
+{
+echo "<script>alert('Invalid username or password');</script>";
+}
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>User Login | Registration and Login System</title>
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-primary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+
+<div class="card-header">
+<h1 align="center">User Login</h1>
+<hr />
+                                    <div class="card-body">
+                                        
+                                        <form method="post">
+                                            
+<div class="form-floating mb-3">
+<input class="form-control" name="uemail" type="email" placeholder="name@example.com" required/>
+<label for="inputEmail">Email address</label>
+</div>
+                                            
+
+<div class="form-floating mb-3">
+<input class="form-control" name="password" type="password" placeholder="Password" required />
+<label for="inputPassword">Password</label>
+</div>
+
+
+<div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+<a class="small" href="#">Forgot Password?</a>
+<button class="btn btn-primary" name="login" type="submit">Login</button>
+</div>
+</form>
+</div>
+                                    <div class="card-footer text-center py-3">
+                                        <div class="small"><a href="signup.php">Need an account? Sign up!</a></div>
+                                          <div class="small"><a href="index.php">Back to Home</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+    </body>
+</html>
